724e67d4cb7a47c0ca21f5abdf8335f515dc3981
nnminh171298 <ngocminh171298@gmail.com>
2019-11-17 21:28:48 +0200

    add WifiScanner structure

---
+++ include/wifiscanner.h
@@ -0,0 +1,36 @@
+#ifndef WIFISCANNER_H
+#define WIFISCANNER_H
+
+#include <string>
+#include <mutex>
+#include <vector>
+
+struct AccessPoint
+{
+	std::string _MAC;
+	int 		_channel;
+	std::string _ESSID;
+	int 		_dbm_strength;
+}
+
+class WifiScanner
+{
+public:
+	WifiScanner() = default;
+	
+	void setInfoFile(const std::string &file_path);
+	void start();
+	
+	std::vector<AccessPoint> getAccessPoints() const;
+	bool isDone() const;
+	
+	std::mutex _mutex;
+
+private:
+	bool _comm_done{true};
+	
+	std::string _info_file_path;
+	const std::string _cmd{"iwlist wlan0 scan | grep -E 'Address|Frequency|ESSID|Quality'"};
+}
+
+#endif // WIFISCANNER_H
