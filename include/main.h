ed3de638bbefc0c7309988c340a35cd8f1273551
nnminh171298 <ngocminh171298@gmail.com>
2019-11-19 15:21:46 +0200

    remove unnecessary headers

--- include/main.h
+++ include/main.h
@@ -1,13 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include "config.h"
-
-#include <string>
 #include <future>
 
-class ServerComm;
-class WifiScanner;
 class Process;
 
 std::future<void> startProcess(Process &process);
