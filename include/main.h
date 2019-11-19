f4f41899087037259f1f454cc5ddc722de24e901
nnminh171298 <ngocminh171298@gmail.com>
2019-11-19 11:32:24 +0200

    clean up main

--- include/main.h
+++ include/main.h
@@ -1,6 +1,49 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "config.h"
 
+#include <string>
+#include <future>
+
+class ServerComm;
+class WifiScanner;
+
+void initWifiScanner(WifiScanner &wifi_scanner, const std::map<ConfigEnum, std::string> &config_map);
+void printWifiScannerResult(WifiScanner &wifi_scanner);
+
+void initServerComm(ServerComm &server_comm, const std::map<ConfigEnum, std::string> &config_map);
+void printServerCommResult(ServerComm &server_comm);
+
+template <typename T>
+std::future<void> startProcess(T &process)
+{
+	auto future = std::async(std::launch::async, &T::start, &process);
+	// wait for the thread to start
+	while(future.wait_for(std::chrono::seconds(0)) == std::future_status::deferred);
+	
+	return future;
+}
+
+template <typename T>
+bool isProcessDone(T &process, const std::string &context)
+{
+	std::unique_lock<std::mutex> lock(process._mutex, std::defer_lock);
+	if(lock.try_lock())
+	{
+		if(process.isDone())
+		{
+			std::cout << "Main: " << context << " done" << std::endl;
+			return true;
+		}
+		else
+			std::cout << "Main: " << context << " still not finished" << std::endl;
+		lock.unlock();
+	}
+	else
+		std::cout << "Main: Cannot lock " << context << std::endl;
+	
+	return false;
+}
 
 #endif // MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include "main.h"
-#include "config.h"
+
 #include "servercomm.h"
 #include "wifiscanner.h"
 
-#include <future>
 #include <thread>
 #include <chrono>
 
@@ -15,65 +14,44 @@ int main()
 	std::cout << std::endl;
 	
 	ServerComm server_comm;
-	std::unique_lock<std::mutex> server_comm_lock(server_comm._mutex);
-	server_comm.setConfigMap(config_map);
-	server_comm_lock.unlock();
-	auto server_comm_future = std::async(std::launch::async, &ServerComm::start, &server_comm);
-	// wait for the thread to start
-	while(server_comm_future.wait_for(std::chrono::seconds(0)) == std::future_status::deferred);
+	std::unique_lock<std::mutex> server_comm_lock(server_comm._mutex, std::defer_lock);
+	initServerComm(server_comm, config_map);
+	auto server_comm_future = startProcess(server_comm);
 	
 	WifiScanner wifi_scanner;
-	std::unique_lock<std::mutex> wifi_scanner_lock(wifi_scanner._mutex);
-	wifi_scanner.setInfoFile(config_map.at(WIFIINFO_PATH));
-	wifi_scanner_lock.unlock();
-	auto wifi_scanner_future = std::async(std::launch::async, &WifiScanner::start, &wifi_scanner);
-	// wait for the thread to start
-	while(wifi_scanner_future.wait_for(std::chrono::seconds(0)) == std::future_status::deferred);
+	std::unique_lock<std::mutex> wifi_scanner_lock(wifi_scanner._mutex, std::defer_lock);
+	initWifiScanner(wifi_scanner, config_map);
+	auto wifi_scanner_future = startProcess(wifi_scanner);
 	
 	while(true)
 	{
-		if(wifi_scanner_lock.try_lock())
+		if(isProcessDone(wifi_scanner, "WifiScanner"))
 		{
-			if(wifi_scanner.isDone())
+			wifi_scanner_lock.lock();
+			if(wifi_scanner.isError())
 			{
-				if(wifi_scanner.isError())
-				{
-					std::cout << "Main: WifiScanner error" << std::endl;
-					wifi_scanner_lock.unlock();
-					break;
-				}
-				
-				std::cout << "Main: WifiScanner done" << std::endl;
-				auto access_points = wifi_scanner.getAccessPoints();
-				for(auto access_point : access_points)
-					std::cout << access_point << std::endl;
+				std::cout << "Main: WifiScanner error" << std::endl;
+				wifi_scanner_lock.unlock();
+				break;
 			}
-			else
-				std::cout << "Main: WifiScanner still not finished" << std::endl;
+			
+			printWifiScannerResult(wifi_scanner);
 			wifi_scanner_lock.unlock();
 		}
-		else
-			std::cout << "Main: Cannot lock WifiScanner" << std::endl;
 		
-		if(server_comm_lock.try_lock())
+		if(isProcessDone(server_comm, "ServerComm"))
 		{
-			if(server_comm.isDone())
+			server_comm_lock.lock();
+			if(server_comm.isError())
 			{
-				if(server_comm.isError())
-				{
-					std::cout << "Main: ServerComm error" << std::endl;
-					server_comm_lock.unlock();
-					break;
-				}
-				std::cout << "Main: ServerComm done" << std::endl;
-				std::cout << "Song name: " << server_comm.getSongName() << std::endl;
+				std::cout << "Main: ServerComm error" << std::endl;
+				server_comm_lock.unlock();
+				break;
 			}
-			else
-				std::cout << "Main: ServerComm still not finished" << std::endl;
+			
+			printServerCommResult(server_comm);
 			server_comm_lock.unlock();
 		}
-		else
-			std::cout << "Main: Cannot lock ServerComm" << std::endl;
 
 		std::this_thread::sleep_for(std::chrono::seconds(2));
 	}
@@ -82,4 +60,30 @@ int main()
 	wifi_scanner_future.wait();
 
 	return 0;
+}
+
+void initWifiScanner(WifiScanner &wifi_scanner, const std::map<ConfigEnum, std::string> &config_map)
+{
+	std::unique_lock<std::mutex> wifi_scanner_lock(wifi_scanner._mutex);
+	wifi_scanner.setInfoFile(config_map.at(WIFIINFO_PATH));
+	wifi_scanner_lock.unlock();
+}
+
+void initServerComm(ServerComm &server_comm, const std::map<ConfigEnum, std::string> &config_map)
+{
+	std::unique_lock<std::mutex> server_comm_lock(server_comm._mutex);
+	server_comm.setConfigMap(config_map);
+	server_comm_lock.unlock();
+}
+
+void printWifiScannerResult(WifiScanner &wifi_scanner)
+{
+	auto access_points = wifi_scanner.getAccessPoints();
+	for(auto access_point : access_points)
+		std::cout << access_point << std::endl;
+}
+
+void printServerCommResult(ServerComm &server_comm)
+{
+	std::cout << "Song name: " << server_comm.getSongName() << std::endl;
 }
