0e38ef497059fff59157be0a4c8e04412f29fc6d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-06 15:03:47 +0000

    Deploy VAMK-embedded-project-2019A/Device-Software to github.com/VAMK-embedded-project-2019A/Device-Software.git:gh-pages

--- navtreedata.js
+++ navtreedata.js
@@ -1,11 +1,17 @@
 var NAVTREE =
 [
   [ "Smart Music Player - Device Software", "index.html", [
-    [ "Data Structures", "annotated.html", [
-      [ "Data Structures", "annotated.html", "annotated_dup" ],
-      [ "Data Structure Index", "classes.html", null ],
+    [ "Device Software", "index.html#autotoc_md0", [
+      [ "Documentation", "index.html#autotoc_md1", null ],
+      [ "System requirements", "index.html#autotoc_md2", null ],
+      [ "Configurations", "index.html#autotoc_md3", null ],
+      [ "Build test", "index.html#autotoc_md4", null ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
       [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Data Fields", "functions.html", [
+      [ "Class Members", "functions.html", [
         [ "All", "functions.html", null ],
         [ "Functions", "functions_func.html", null ],
         [ "Enumerations", "functions_enum.html", null ]
--- search/searchdata.js
+++ search/searchdata.js
@@ -17,7 +17,7 @@ var indexSectionNames =
 var indexSectionLabels =
 {
   0: "All",
-  1: "Data Structures",
+  1: "Classes",
   2: "Functions",
   3: "Enumerations"
 };
