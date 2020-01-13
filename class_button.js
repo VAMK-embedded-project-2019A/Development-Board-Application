e42c7f07674870512cec1fcda07a7ab66bf065f0
nnminh171298 (via Travis CI) <nnminh171298@gmail.com>
2020-01-13 15:46:21 +0000

    Deploy VAMK-embedded-project-2019A/Development-Board-Application to github.com/VAMK-embedded-project-2019A/Development-Board-Application.git:gh-pages

--- class_button.js
+++ class_button.js
@@ -1,9 +1,14 @@
 var class_button =
 [
     [ "Button", "class_button.html#a8a98595f64fac75a8a01c6231047e15a", null ],
+    [ "Button", "class_button.html#a09b6b346a95f384f15ef9f141edf6946", null ],
+    [ "Button", "class_button.html#ab4c8821b94e223682356afe4045db7b3", null ],
+    [ "~Button", "class_button.html#a2a001eb9c3cc8ae54768a850dd345002", null ],
     [ "Button", "class_button.html#a08316cad279e874532cbdd7b701d8a15", null ],
     [ "closeFd", "class_button.html#a08188f5e677454c9b11e6a6393052116", null ],
     [ "openFd", "class_button.html#ab77649093bbc71abe2c1ffe6f22708d3", null ],
+    [ "operator=", "class_button.html#a2055c914d5b7b143cc2c4d0a8d5a29cb", null ],
+    [ "operator=", "class_button.html#a32c9518ea98c05026a3fc9a6ea544d2d", null ],
     [ "pinExport", "class_button.html#a9c8634ff6f161f9d637012a7fadc6883", null ],
     [ "setDirection", "class_button.html#ad80556212b1ce493542077486d335786", null ],
     [ "setEdge", "class_button.html#a554c41fa4b6ad04925e780cc32c3b898", null ],
